45e1eca8b0b9773b06e9c314681a7e83bfc1dc7f
Ryan Hodin <ryan951357@gmail.com>
2021-08-08 15:25:47 -0500

    Working blog item display

--- src/components/FullBlogItem.vue
+++ src/components/FullBlogItem.vue
@@ -1,44 +1,36 @@
 <template>
-  <div class="host">
-    <div class="left">
-      <a v-bind:href="'tel:' + phoneNumber"
-        ><q-icon name="phone" />{{ phoneNumber }}</a
-      >
-    </div>
-    <div class="right">
-      <a v-bind:href="'mailto:' + email"><q-icon name="mail" />{{ email }}</a>
-    </div>
-    <div class="left">
-      <a v-bind:href="discordURL"><q-icon name="fab fa-discord" />za419#0014</a>
-    </div>
-    <div class="right">
-      <a v-bind:href="githubURL"><q-icon name="fab fa-github" />za419</a>
-    </div>
-    <div class="left">
-      <a v-bind:href="facebookURL"
-        ><q-icon name="fab fa-facebook" />Ryan Hodin</a
-      >
-    </div>
-    <div class="right">
-      <a v-bind:href="linkedinURL"
-        ><q-icon name="fab fa-linkedin" />Ryan Hodin</a
-      >
-    </div>
-  </div>
+  <div class="host" v-html="text"></div>
 </template>
 
 <script lang="ts">
+import blogRenderer from "../blogToHtml";
+import blogListing from "../assets/blog/listing.json";
+
+const data = { text: "Loading..." };
+
+// Return the current HTML we should be displaying
+async function getContents(): Promise<string> {
+  await new Promise((r) => setTimeout(r, 3000));
+  // Get the blog item. If it doesn't exist, print an error.
+  const item = blogListing.find((blog) => blog.id === 0);
+  if (!item) {
+    return '<div class="error">Blog with id 0 does not exist.</div>';
+  }
+
+  const result = await blogRenderer(item);
+
+  console.log(result);
+
+  return result;
+}
+
+// When we can get the contents downloaded, place them in the component.
+getContents().then((text) => (data.text = text));
+
 export default {
-  name: "ContactSection",
+  name: "FullBlogItem",
   data: (): Record<string, unknown> => {
-    return {
-      phoneNumber: "(012)345-6789",
-      email: "address@example.com",
-      discordURL: "https://discord.com/users/195682839998627840",
-      githubURL: "https://github.com/za419",
-      facebookURL: "/",
-      linkedinURL: "https://www.linkedin.com/in/ryan-hodin-902191116/",
-    };
+    return data;
   },
 };
 </script>
