5f6cc1da562f43e39c3f3a56c447bc342ea8e7e8
Ryan Hodin <ryan951357@gmail.com>
2021-09-12 01:11:30 -0500

    Add about page contents
    I wrote this up using the live blogpost preview/development tool,
    although I cleaned up the HTML after it rendered. My generator still
    outputs pretty... Awkward HTML sometimes. I also stripped out the "Blog"
    references, since this isn't one.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,83 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h4 class="title">About Ryan Hodin</h4>
+    I'm not very good at writing about myself. Really.
+    <p>
+      If you don't believe me, here's an example - An edited version of an
+      introduction I wrote a while ago.
+    </p>
+    <hr />
+    I'm a 24-year-old software engineer working and living in Chicago - I
+    graduated from Northwestern University with a degree in computer science.
+    <p>
+      Before college, I lived in Reno, NV, and I was born in Las Vegas. Both of
+      my parents were immigrants (from different parts of the world no less), so
+      I'm a first-generation American.
+    </p>
+    <hr />
+    See, like I said - I'm pretty bad at this.<br />
+    So let's try a different approach. Here's a list of facts about me, in no
+    particular order:
+    <p></p>
+    <ul class="unordered-list">
+      <li>I own a <strong>lot</strong> of plants.</li>
+      <li>
+        Most of my plants are <em>Tillandsia</em>, otherwise known as air
+        plants.
+      </li>
+      <li>
+        I also own some carnivorous plants - Two 'B52' cultivar Venus Flytraps,
+        and two sundews.
+      </li>
+      <li>I'm running out of things to say...</li>
+      <li>
+        I've decided to leave it off here even though it's super lame, and come
+        back if I think of more things.
+      </li>
+      <li>If you see this, it means I never came back with more things.</li>
+    </ul>
+    <h5 class="subtitle">About this website</h5>
+    This is my personal website. A big part of why I made it was because I heard
+    of
+    <a href="https://vuejs.org/" title="Vue.js">Vue.js</a>, and I wanted to give
+    it a try, and maybe also refresh my (extremely rusty and rather minimal) web
+    development skillset. Seriously, last time I made a website on my own, it
+    was using absolutely no framework, targeting pre-ES5 Javascript and CSS2.<br />
+    It was bad. And I hated working on it, so it never got finished enough to go
+    up on the open internet.
+    <p>
+      This site is developed with Vue, in Typescript and SCSS, and it uses
+      <a href="https://quasar.dev/" title="Quasar">Quasar</a>
+      as a design framework. So it's much nicer looking, I get more done per
+      hour of work, and the whole thing is generally a better idea.
+    </p>
+    <p>
+      Ultimately though, the point is for me to learn things. Like the blog
+      generation system I wrote, which is entirely client-side, uses a custom
+      language, and is generally worse than statically processed Markdown in
+      pretty much every way, but I thought it would be an interesting exercise
+      to create such a thing, so I did. And maybe you'll enjoy yourself toying
+      with this site too!
+    </p>
   </div>
 </template>
+
+<style>
+.about {
+  width: 80%;
+  margin: auto;
+}
+
+p {
+  margin-top: 1em;
+}
+
+h4.title,
+h5 .subtitle {
+  margin: 1em 0;
+}
+
+h4 {
+  margin: 1em 0;
+}
+</style>
