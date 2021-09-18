29c876ef450949d3617b281d2fb262105a57188e
Ryan Hodin <ryan951357@gmail.com>
2021-09-17 20:12:08 -0500

    Write informal forward to the full privacy policy
    This is a simple, no-BS way to communicate what I do here.

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -2,6 +2,29 @@
   <div class="privacy-policy-root">
     <h2 class="title">Privacy Policy</h2>
     <div class="publish-date">Last updated: {{ lastUpdated }}</div>
+    <div class="foreword">
+      In plain terms, I do not collect <strong>any</strong> information from you
+      when you visit this site.
+      <p>
+        In full (informal) terms: This site is hosted on Github Pages, which
+        does not allow me to save any data on the server. It does not connect to
+        any other server, except to fetch resources such as images - No data is
+        sent from this site to any location I have control over.
+      </p>
+      <p>
+        Pages also does not provide me with any statistics regarding who is
+        viewing this site, or how many times it is viewed. I do not inject
+        Google Analytics or another script which would collect that information
+        from your browser either - In short, I know nothing more about you after
+        you've seen this site compared to before you came here, neither as an
+        individual or aggregated into a pool of however many people have seen
+        this (I don't even know that much!).
+      </p>
+      <p>
+        A more detailed, more formal description of this arrangement follows
+        below:
+      </p>
+    </div>
     <hr />
   </div>
 </template>
@@ -35,9 +58,20 @@ export default Vue.extend({
     margin-top: 1em;
   }
 
-  h2.title,
+  h2.title {
+    margin-top: 1em;
+    margin-bottom: 0.35em;
+  }
+
   div.publish-date {
-    margin: 1em 0;
+    margin-bottom: 2em;
+    font-size: 0.95em;
+    font-weight: 300;
+    color: rgba(0, 0, 0, 0.8);
+  }
+
+  div.foreword {
+    margin-bottom: 1em;
   }
 }
 
