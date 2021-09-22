f9efc6befb5caa6cbf2d4698bd347838bd6f3e0a
Ryan Hodin <ryan951357@gmail.com>
2021-09-21 22:03:12 -0500

    Add a title to the blog list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="home">
-    <ul class="blog-preview-list">
-      <li v-for="id in blogIDs" :key="id" class="blog-preview-list-root">
-        <BlogItemPreview :id="id" navigationRoot="/blog/" />
-      </li>
-    </ul>
+    <div class="blog-preview-container">
+      <h2 class="blog-preview-title">Blog</h2>
+      <ul class="blog-preview-list">
+        <li v-for="id in blogIDs" :key="id" class="blog-preview-list-root">
+          <BlogItemPreview :id="id" navigationRoot="/blog/" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,19 +27,34 @@ export default class Home extends Vue {
 </script>
 
 <style lang="scss">
-.blog-preview-list {
-  display: inline-block;
-  height: 180px;
-  overflow-x: hidden;
-  overflow-y: auto;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
+.home {
+  .blog-preview-container {
+    display: inline-block;
 
-  li.blog-preview-list-root {
-    width: 100%;
-    height: 90px;
-    display: block;
+    .blog-preview-title {
+      font-size: 2.5rem;
+      font-weight: 600;
+      line-height: 2.5rem;
+      text-align: center;
+      margin-top: 0;
+      margin-bottom: 0.5rem;
+    }
+
+    .blog-preview-list {
+      display: inline-block;
+      height: 180px;
+      overflow-x: hidden;
+      overflow-y: auto;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+
+      li.blog-preview-list-root {
+        width: 100%;
+        height: 90px;
+        display: block;
+      }
+    }
   }
 }
 </style>
