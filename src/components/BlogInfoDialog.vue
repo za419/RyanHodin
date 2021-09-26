e98aa7600cae24732bedb82d5d9ace04c69f0887
Ryan Hodin <ryan951357@gmail.com>
2021-09-26 17:20:20 -0500

    Add title stylings for the dialog
    These don't really resemble what we do in the actual blog render, but it
    gets the point across.

--- src/components/BlogInfoDialog.vue
+++ src/components/BlogInfoDialog.vue
@@ -28,5 +28,29 @@ export default {
   .blog-info-dialog-title {
     margin: 0.75em 0;
   }
+
+  // From here down, we're duplicating FullBlogItem's styles, except custom for our dialog.
+  .blog-specification-list-item {
+    h4.blog-text-title {
+      display: inline-block;
+      margin: 0 1ch;
+      font-size: 1.75em;
+      line-height: 1.75em;
+    }
+
+    h5.blog-text-subtitle {
+      display: inline-block;
+      margin: 0 1ch;
+      font-size: 1.5em;
+      line-height: 1.5em;
+    }
+
+    h6.blog-text-minor-title {
+      display: inline-block;
+      margin: 0 1ch;
+      font-size: 1.15em;
+      line-height: 1.15em;
+    }
+  }
 }
 </style>
