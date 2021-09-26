df4cf3a0140ffed2464142ad880b8a2629d66246
Ryan Hodin <ryan951357@gmail.com>
2021-09-26 17:02:41 -0500

    Add component for basic information on blog styles
    This will provide some basic, more easily accessible information on how
    to use the language.
    
    This is a rare case of me legitimately using Quasar properly - This will
    be a little more consistent in design (I hope)

---
+++ src/components/BlogInfoDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-card class="blog-info-dialog-card">
+    <q-card-section>
+      <h3>Blog Item Styles:</h3>
+      The following styles are available in blog posts:
+    </q-card-section>
+    <q-separator />
+    <q-card-section>
+      {{ informationList }}
+    </q-card-section>
+    <q-separator />
+    <q-card-actions><q-btn flat>More information</q-btn></q-card-actions>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { blogItemUsageList } from "@/blogToHtml";
+export default {
+  name: "BlogInfoDialog",
+  computed: {
+    informationList(): string {
+      return blogItemUsageList();
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
